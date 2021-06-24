8adfbf1e4c7cad022fddc54006d4d005c3137581
aminoo10 <acminoo95@gmail.com>
2021-06-23 19:08:51 -0700

    reinstalled sass module (was broken)

--- src/About.js
+++ src/About.js
@@ -3,6 +3,11 @@
 const About = () => {
   return (
     <div className="about">
+      <figure>
+        <blockquote><i>“Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away.”</i></blockquote>
+        <figcaption><small>-Antoine de Saint-Exupéry</small></figcaption>
+      </figure>
+
       <h1>About Me</h1>
       <img src="/images/me.jpg" alt="me wearing cool sunglasses" title="me wearing cool sunglasses"/>
       <p>Ashcon Minoo was born in June 1995, he graduated with a degree in <strong><i>Computer Science and Systems</i></strong> from the <i>University of Washington Tacoma</i> in 2017. During the COVID-19 Pandemic Ashcon got a certificate from the same school for <strong><i>FRONT-END DEVELOPMENT WITH HTML, CSS and JAVASCRIPT</i></strong>. Working on and off with freelance art in between jobs, and currently studying to learn more abaout front-end Development and design.<br/><br/> Ashcon loves to draw and has a great interest in art, animation, cartoons, comics, and movies. He also considers himself a hardcore gamer, with his favorite games being: Super Smash Brothers Melee, Quake 3 Arena, and Dark Souls. His favorite game of all time is Super Metroid for the Super Nintendo Entertainment System</p>
