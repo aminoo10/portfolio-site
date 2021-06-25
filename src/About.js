72a61f37dfd8d6190ec9388e36c67fbedae0955e
aminoo10 <acminoo95@gmail.com>
2021-06-24 22:50:43 -0700

    revised about page and resume page

--- src/About.js
+++ src/About.js
@@ -10,7 +10,11 @@ const About = () => {
 
       <h1>About Me</h1>
       <img src="/images/me.jpg" alt="me wearing cool sunglasses" title="me wearing cool sunglasses"/>
-      <p>Ashcon Minoo was born in June 1995, he graduated with a degree in <strong><i>Computer Science and Systems</i></strong> from the <i>University of Washington Tacoma</i> in 2017. During the COVID-19 Pandemic Ashcon got a certificate from the same school for <strong><i>FRONT-END DEVELOPMENT WITH HTML, CSS and JAVASCRIPT</i></strong>. Working on and off with freelance art in between jobs, and currently studying to learn more abaout front-end Development and design.<br/><br/> Ashcon loves to draw and has a great interest in art, animation, cartoons, comics, and movies. He also considers himself a hardcore gamer, with his favorite games being: Super Smash Brothers Melee, Quake 3 Arena, and Dark Souls. His favorite game of all time is Super Metroid for the Super Nintendo Entertainment System</p>
+      <p>Ashcon Minoo was born in June 1995, he graduated with a degree in <strong><i>Computer Science and Systems</i></strong> from the <i>University of Washington Tacoma</i> in 2017. 
+      During the COVID-19 Pandemic Ashcon got a certificate from the same school for <strong><i>FRONT-END DEVELOPMENT WITH HTML, CSS and JAVASCRIPT</i></strong>. 
+      Has worked in freelance art, and currently studying to learn more about Front-End Development and Design.<br/><br/> 
+      Ashcon enjoys drawing as a hobby and enjoys animation, cartoons, comics, and movies.</p>
+      
       <img src="/images/more.jpg" alt="harajuku" title="trapped within the system"/>
     </div>
     );
