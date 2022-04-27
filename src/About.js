d89d387450ce6268c6d702ac840864b99423fb3f
aminoo10 <acminoo95@gmail.com>
2022-04-27 03:33:43 -0700

    changes to about page and updated resume

--- src/About.js
+++ src/About.js
@@ -11,7 +11,7 @@ const About = () => {
       <p>I  graduated with a degree in <strong><i>Computer Science and Systems</i></strong> from the <i>University of Washington Tacoma</i> in 2017. 
       During the COVID-19 Pandemic, I got a certificate from the same school for <strong><i>FRONT-END DEVELOPMENT WITH HTML, CSS and JAVASCRIPT</i></strong>. 
       <br/><br/>In between jobs I've worked in freelance art, and currently am studying to learn more about Front-End Development, Android Development, and Design<br/><br/> 
-      In my free time I enjoy drawing as a hobby and have interests in animation, cartoons, comics, and movies. I also like video games too and often am brainstorming video game concepts.</p>
+      In my free time I enjoy drawing as a hobby and have interests in animation, cartoons, comics, and movies. I also like video games too and often am brainstorming video game concepts together with a few of my friends.</p>
       
       <img src="/images/more.jpg" alt="harajuku" title="trapped within the system"/>
     </div>
--- src/Resume.js
+++ src/Resume.js
@@ -10,18 +10,27 @@ const Resume = () => {
         <p>acminoo95@gmail.com <br />(253)951-1134 <br /><a href="https://github.com/aminoo10" target="_blank" rel="noreferrer">https://github.com/aminoo10</a></p>
         <hr />
         <h2>Education</h2>
-        <p><i>University of Washington Tacoma - <strong>Bachelor of Science in Computer Science &amp; Systems</strong></i></p>
-        <p><i>University of Washington - <strong>Certificate in Front-End Web Development</strong></i></p>
+        <p><i>University of Washington Tacoma - <strong>Bachelor of Science in Computer Science &amp; Systems (2015-2017)</strong></i></p>
+        <p><i>University of Washington - <strong>Certificate in Front-End Web Development (2020-2021)</strong></i></p>
         <hr />
         <h2>Skills &amp; Experience</h2>
         <ul>
-          <li><p>Experience and exposure to languages in Object-Oriented Design: C, Java, SQL, and Javascript</p></li>
+          <li><p>Experience and exposure to languages in Object-Oriented Design: <strong>C, Java</strong>, SQL, and Javascript</p></li>
           <li><p>Experience in web development utilizing HTML, CSS, JS, Jquery, preprocessors (sass), and frameworks (Vue/Nuxt). Continuing to expand knowledge with other frameworks and technologies (React/Nuxt.js), exposure to C# and Python.</p></li>
           <li><p>Fast learner, adaptive, proactive. Project development experience through AGILE workflow.</p></li>
+          <li><p>Experience working with cloud technologies such as Azure, as well as work as a Database Administrator with SQL. Exposure to AWS.</p></li>
         </ul>
         <hr />
+        <h2>Project Experience</h2>
+        <h3>Personal Projects:</h3>
+        <p>Programming projects done in my spare time for learning/mastering new technologies: Jan 2021 - Present</p>
+        <h4>"Front-end development API project"</h4>
+        <p>A project using the <strong>Nuxt/Vue framework, Javascript, HTML, CSS</strong> that fetches requests through an API with the purpose of querying and listing information on Anime and Manga title listings.</p>
+        <h4>"Portfolio Website"</h4>
+        <p>A portfolio website made using the <strong>ReactJS</strong> framework, Javascript, HTML, and CSS. Made to showcase other web applications I've made and other resources for professional use.  <em>(You're on it right now!!!)</em></p>
+        <hr />
         <h2>Work Experience</h2>
-        <h3>IT Internship at Weyerhaeuser</h3> 
+        <h3>IT Internship at Weyerhaeuser:</h3> 
         <p>Seattle, WA.  <strong>Working in three roles:</strong> Jan 2019 - Jan 2020</p>
         <h4>Database Administration:</h4>
         <p>Part of the DBA team working on installing and setting up <strong>SQL Server</strong>, Database migrations, and database/server functions per service request by application stewards and corporate function development teams</p>
@@ -33,12 +42,7 @@ const Resume = () => {
           <li><p>Coordinating with project managers and developers on acceptance criterias, functionality, and bug documentation</p></li>
         </ul>
         <hr />
-        <h2>Project Experience</h2>
-        <h3>"Audit Logger"</h3>
-        <p>Developed an Azure logic app to save audit records of accounts and subscriptions accessing Azure DevOps into Excel spreadsheets, and stored into an Azure storage account at the end of each day. This was extremely beneficial in the team and organization’s security and auditing of control systems.</p>
-        <h3>"Front-end development API project"</h3>
-        <p>A project using the <strong>Nuxt/Vue framework, Javascript, HTML, CSS</strong> that fetches requests through an API with the purpose of querying and listing information on Anime and Manga title listings.</p>
-        <p><strong>Key Courses:</strong> Data Structures, <strong>C Programming</strong>, Design and Analysis of Algorithms, <strong>Software Development and QA, Database Systems</strong>, User-Computer Design Interface, Computational Worlds, <strong>Javascript and Advanced CSS.</strong></p>
+        {/* <p><strong>Key Courses:</strong> Data Structures, <strong>C Programming</strong>, Design and Analysis of Algorithms, <strong>Software Development and QA, Database Systems</strong>, User-Computer Design Interface, Computational Worlds, <strong>Javascript and Advanced CSS.</strong></p> */}
       </div>}
       { usePDF && <div className="pdf-resume">
         <iframe title="My Resume" src={`${PDF}#toolbar=0&navpanes=0&scrollbar=0`} width="65%" height="1070px"> </iframe>
